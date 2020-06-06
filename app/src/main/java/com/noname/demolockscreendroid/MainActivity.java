dca5bea5ebbf99721879e4773026d4744a57d26d
5y573m3rr0r <63899148+5y573m3rr0r@users.noreply.github.com>
2020-06-06 18:49:13 +0600

    Ac tionbar as toolbar added successfully

--- app/src/main/java/com/noname/demolockscreendroid/MainActivity.java
+++ app/src/main/java/com/noname/demolockscreendroid/MainActivity.java
@@ -1,14 +1,22 @@
 package com.noname.demolockscreendroid;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.ToolbarWidgetWrapper;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar customizedToolbar = (Toolbar) findViewById(R.id.action_toolbar);
+        setSupportActionBar(customizedToolbar);
+
     }
 }
